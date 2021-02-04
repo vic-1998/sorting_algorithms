3e430118ded7e393bdfd6fbd04e79484dab8d2e9
vic-1998 <victor.mplaza98@gmail.com>
2021-02-04 03:43:46 +0000

    Add funtion merge_sort.

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
