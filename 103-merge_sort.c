95e019066f5dc16e081d8ae7a0ac9d8e66bb6f64
vic-1998 <victor.mplaza98@gmail.com>
2021-02-04 04:00:14 +0000

    Add funtiom merge_sort_h to file.

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 
+
+void merge_sort_h(size_t start, size_t end, int *array, int *copy)
+{
+        size_t middle;
+        if (end - start < 2)
+                return;
+        middle = (start + end) / 2;
+merge_sort_h(start, middle, array, copy);
+        merge_sort_h(middle, end, array, copy);
+        merge(start, middle, end, array, copy);
+        for (middle = start; middle < end; middle++)
+        copy[middle] = array[middle];
+}
+
 /**
  * merge_sort - sorts an array of integers in ascending order using
  * the Merge sort algorithm
