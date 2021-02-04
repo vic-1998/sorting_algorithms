10598e4f4831cc9ca7dc4cd81111965631f3c60d
vic-1998 <victor.mplaza98@gmail.com>
2021-02-04 03:58:19 +0000

    Add comments to the funtion merge sort.

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,18 +1,24 @@
 #include "sort.h"
 
-
+/**
+ * merge_sort - sorts an array of integers in ascending order using
+ * the Merge sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Return: void
+ */
 void merge_sort(int *array, size_t size)
 {
-        size_t i;
-        int *copy;
+	size_t i;
+	int *copy;
 
-        if (array == NULL || size < 2)
-                return;
-        copy = malloc(sizeof(int) * size);
-        if (copy == NULL)
-                return;
-        for (i = 0; i < size; i++)
-                copy[i] = array[i];
-        merge_sort_h(0, size, array, copy);
-        free(copy);
+	if (array == NULL || size < 2)
+		return;
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		copy[i] = array[i];
+	merge_sort_h(0, size, array, copy);
+	free(copy);
 }
