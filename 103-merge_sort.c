9450be8061a6bd1b8aaabb2aabc9917b350b3d8c
vic-1998 <victor.mplaza98@gmail.com>
2021-02-04 03:54:26 +0000

    Adding prototype and function building.

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,11 +1,18 @@
 #include "sort.h"
 
 
-
-
-
-
 void merge_sort(int *array, size_t size)
 {
-        
+        size_t i;
+        int *copy;
+
+        if (array == NULL || size < 2)
+                return;
+        copy = malloc(sizeof(int) * size);
+        if (copy == NULL)
+                return;
+        for (i = 0; i < size; i++)
+                copy[i] = array[i];
+        merge_sort_h(0, size, array, copy);
+        free(copy);
 }
